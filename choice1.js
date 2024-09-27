8d1cb836b5ff1ed5a27940d39bd2a5ab27fd4357
Amanda Ventura <saenz242ever@gmail.com>
2024-09-26 22:52:57 -0600

    made changes to functionality, and html for choice 1 questions

--- choice1.js
+++ choice1.js
@@ -1,22 +1,22 @@
 function getSelectedAnswer(event){
     event.preventDefault();
     if (event.target.value === "business") {
-      alert("The business is profitable but attracts unwanted attention from the authorities. Eventually, it is shut down, and you lose your investment.");
+      alert("The business is profitable but attracts unwanted attention from the authorities. Eventually, it is shut down, and you lose your investment. Consequence: The covert business option can be lucrative short-term but risky long-term, potentially leading to severe consequences.");
     }
     if (event.target.value === "aid") {
-      alert("The aid is inconsistent, and your family faces constant uncertainty, but you avoid legal trouble. Consequence: The covert business option can be lucrative short-term but risky long-term, potentially leading to severe consequences.");
+      alert("The aid is inconsistent, and your family faces constant uncertainty, but you avoid legal trouble.");
     }
     if (event.target.value === "advocate") {
         alert("Your activism draws attention, and you face harassment, but you build solidarity with other Palestinians. Consequence: Advocacy can lead to harassment and potential violence, while staying quiet can erode your family's morale and future prospects.");
     }
     if (event.target.value === "profile") {
-        alert("Your family avoids direct conflict, but the discrimination takes a toll on your children's mental health and self-esteem.");
+        alert("Your family avoids direct conflict, but the discrimination takes a toll on your children's mental health and self-esteem. Consequence: Advocacy can lead to harassment and potential violence, while staying quiet can erode your family's morale and future prospects.");
     }
     if (event.target.value === "palestine") {
         alert("Your family continues to face challenges but remains committed to your homeland. Consequence: Staying might continue the cycle of hardship, while leaving could result in losing your identity and roots.");
     }
     if (event.target.value === "leave") {
-        alert("You start anew, but your family loses its connection to the land and its heritage.");
+        alert("You start anew, but your family loses its connection to the land and its heritage. Consequence: Staying might continue the cycle of hardship, while leaving could result in losing your identity and roots.");
     }
   }
   //selector for form
